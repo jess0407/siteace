f694fc5a2c5aaddc4b33b04c5ad17d50b4504cd6
Jessie Shi <jessie.shi@me.com>
2016-03-07 13:32:35 +0000

    User can upvote or downvote a site

--- siteace.js
+++ siteace.js
@@ -20,22 +20,17 @@ if (Meteor.isClient) {
 
 	Template.website_item.events({
 		"click .js-upvote":function(event){
-			// example of how you can access the id for the website in the database
-			// (this is the data context for the template)
-			var website_id = this._id;
-			console.log("Up voting website with id "+website_id);
-			// put the code in here to add a vote to a website!
 
+			Websites.update(this._id, {
+		    	$inc: {votes: 1}
+		    });
 			return false;// prevent the button from reloading the page
 		},
 		"click .js-downvote":function(event){
 
-			// example of how you can access the id for the website in the database
-			// (this is the data context for the template)
-			var website_id = this._id;
-			console.log("Down voting website with id "+website_id);
-
-			// put the code in here to remove a vote from a website!
+			Websites.update(this._id, {
+		    	$inc: {votes: -1}
+		    });
 
 			return false;// prevent the button from reloading the page
 		}
